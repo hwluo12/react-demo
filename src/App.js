f96d3dd556704c4dafce4bc86d4e3c82316bb4f2
hwluo12.shijingshan <hwluo12@163.com>
2020-02-25 14:20:01 +0800

    引用react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Layout } from "antd";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 
 const { Header, Content } = Layout;
 
 function App() {
   return (
-    <Layout>
-      <Header style={{ color: "#fff" }}>Header</Header>
-      <Content style={{ padding: "20px 20px" }}>
-        <Home />
-      </Content>
-    </Layout>
+    <Router>
+      <Layout>
+        <Header style={{ color: "#fff" }}>Header</Header>
+        <Content style={{ padding: "20px 20px" }}>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Content>
+      </Layout>
+    </Router>
   );
 }
 
