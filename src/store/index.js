1a17703f4709b54bb47e278dd9df38b16b4830b8
hwluo12.shijingshan <hwluo12@163.com>
2020-02-25 19:11:54 +0800

    安装thunk并引入到store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import reducer from "./reducer";
 
-let store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+let store = createStore(reducer, enhancer);
 
 export default store;
